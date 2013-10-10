febd74fbc01b97a4e444f4ebfde8dd556f871d42
Jeff Hui <jeff@jeffhui.net>
2013-10-10 00:48:59 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='RPICourses',
     description='A data scraping library to read course data from RPI.',
     license='MIT',
-    version='1.0.6',
+    version='1.0.7',
     author='Jeff Hui',
     author_email='jeff@jeffhui.net',
     url='https://github.com/jeffh/rpi_courses',
